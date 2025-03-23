c5111f461d28e6388f6e15608a7c6a06e5f94711
MukeshJangid17 <mukeshjangid7877@gmail.com>
2025-03-23 16:04:10 +0530

    experimented with HF_api

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,4 +1,4 @@
-from langchain_core.prompts import PromptTemplate,load_prompt
+from langchain_core.prompts import load_prompt
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import os
@@ -7,7 +7,7 @@ import streamlit as st
 load_dotenv()
 
 # Hugging Face API token
-hf_api_token = os.getenv("HUGGINGFACE_API_TOKEN")
+hf_api_token = os.getenv("Summarizer_api")
 
 llm =  HuggingFaceEndpoint(
     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
@@ -18,12 +18,25 @@ llm =  HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm)
 
 
-st.header("Reaserch Tool")
+st.header("Research Tool")
 
 paper_input = st.selectbox("Select a research paper", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"])
 
 style_input = st.selectbox("Select a style", ["begineer friendly", "Technical", "Code-Oriented", "Casual"])
 
-length_input = st.select_box("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+length_input = st.selectbox("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
+template = load_prompt('template.json')
+
+# fill the place holder 
+
+prompt = template.invoke({
+    'paper_input': paper_input,
+    'style_input': style_input,
+    'length_input': length_input
+})
+
+if st.button("Summarize"):
+    result = model.invoke(prompt)
+    st.write(result.content)
 
